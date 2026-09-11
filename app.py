21f3f09cf19f7a3cd43ac8f66c65949c302790fd
rfynuriy <nurrohimraffi0@gmail.com>
2026-09-11 19:28:59 +0700

    add enum and table users

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Enum as SQLEnum
 
 #enum
 from enum import Enum
@@ -39,3 +40,18 @@ db_secret_key = os.getenv("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
 app.config['SECRET_KEY'] = db_secret_key
 db = SQLAlchemy(app)
+
+class UserRole(Enum):
+    ADMIN = "admin"
+    CUSTOMER = "customer"
+    SELLER = "seller"
+
+class Users(db.Model):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(700), unique=True)
+    phone_number: Mapped[str] = mapped_column(String(20), unique=True, nullable=False)
+    role: Mapped[UserRole] = mapped_column(SQLEnum(UserRole, name="user_role_enum"), default=UserRole.CUSTOMER)
